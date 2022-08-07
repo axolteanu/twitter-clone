936de9e97ce18c6cf85364945560d2378166e0c1
axolteanu <alex.m.olteanu@gmail.com>
2022-08-07 17:52:25 -0400

    Use memo hook to prevent recalculating month and year options on every render.

--- views/react/DobSelect.js
+++ views/react/DobSelect.js
@@ -1,11 +1,19 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useEffect, useMemo, useRef, useState } from 'react';
 import './DobSelect.css'
-
-const nYearOptions = 120;
 
 export function DobSelect(props){
   const daySelect = useRef();
+  const [nMonths] = useState(12);
   const [nDays, setNDays] = useState(31);
+  const [nYears] = useState(120);
+
+  function getNumberOfDaysInMonth(year, month){
+    if(!month)
+      month = 0;
+    if(!year)
+      year = 0;
+    return (new Date(year, month, 0)).getDate();
+  }
 
   useEffect(() => {
     if(props.dobDay){
@@ -21,26 +29,8 @@ export function DobSelect(props){
       daySelect.current.dispatchEvent(new Event('change', {bubbles:true}));
     }
   }, [nDays]);
-
-  function getNumberOfDaysInMonth(year, month){
-    if(!month)
-      month = 0;
-    if(!year)
-      year = 0;
-    return (new Date(year, month, 0)).getDate();
-  }
-
-  function onFocusSelect(e){
-    e.target.style.outline = '2px solid rgb(29,155,240)';
-    e.target.parentElement.children[1].style.color = 'rgb(29,155,240)';
-  }
-
-  function onBlurSelect(e){
-    e.target.style.outline = 'none';
-    e.target.parentElement.children[1].style.color = 'rgb(72,72,72)';
-  }
   
-  function getMonthOptions(){
+  const monthOptions = useMemo(() => {
     return (
       <React.Fragment>
         <option value=""></option>
@@ -58,24 +48,34 @@ export function DobSelect(props){
         <option value="12">December</option>
       </React.Fragment>
     );
-  }
+  }, [nMonths]);
 
-  function getDayOptions(){
+  const dayOptions = useMemo(() => {
     let day = 1;
     return [<option key="0" value=""/>, ...[...Array(nDays)].map(() => {
       let option = <option key={day} value={day}>{day}</option>;
       day++;
       return option;
     })];
-  }
+  }, [nDays]);
 
-  function getYearOptions(){
+  const yearOptions = useMemo(() => {
     let year = (new Date()).getFullYear();
-    return [<option key="0" value=""/>, ...[...Array(nYearOptions)].map(() => {
+    return [<option key="0" value=""/>, ...[...Array(nYears)].map(() => {
       let option = <option key={year} value={year}>{year}</option>;
       year--;
       return option;
     })];
+  }, [nYears]);
+
+  function onFocusSelect(e){
+    e.target.style.outline = '2px solid rgb(29,155,240)';
+    e.target.parentElement.children[1].style.color = 'rgb(29,155,240)';
+  }
+
+  function onBlurSelect(e){
+    e.target.style.outline = 'none';
+    e.target.parentElement.children[1].style.color = 'rgb(72,72,72)';
   }
 
   return (
@@ -86,7 +86,7 @@ export function DobSelect(props){
           value={props.dobMonth || ""}
           onFocus={onFocusSelect} 
           onBlur={onBlurSelect}
-          onChange={props.onChange}>{getMonthOptions()}</select>
+          onChange={props.onChange}>{monthOptions}</select>
         <label>Month</label>
       </div>
       <div>
@@ -96,7 +96,7 @@ export function DobSelect(props){
           value={props.dobDay || ""}
           onFocus={onFocusSelect} 
           onBlur={onBlurSelect}
-          onChange={props.onChange}>{getDayOptions()}</select>
+          onChange={props.onChange}>{dayOptions}</select>
         <label>Day</label>
       </div>
       <div>
@@ -105,7 +105,7 @@ export function DobSelect(props){
           value={props.dobYear || ""}
           onFocus={onFocusSelect} 
           onBlur={onBlurSelect}
-          onChange={props.onChange}>{getYearOptions()}</select>
+          onChange={props.onChange}>{yearOptions}</select>
         <label>Year</label>
       </div>
     </div>
