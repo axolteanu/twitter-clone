4de123684cb08173f9cef7fd50395e00a920c35d
axolteanu <alex.m.olteanu@gmail.com>
2022-08-19 18:09:17 -0400

    Add Tweet form to Home page with multiline textarea.

--- views/react/Home.js
+++ views/react/Home.js
@@ -1,14 +1,46 @@
 import ReactDOM from 'react-dom/client';
-import React from 'react';
+import React, { useEffect } from 'react';
+import './Home.css';
 
 function Home(props){
+  
+  useEffect(() => {
+    let ta = document.getElementById('ta');
+    let taSib = document.getElementById('ta-sib');
+    if(!ta.value)
+    taSib.style.height = ta.clientHeight + 'px';
+  });
+
+  function onChange(e){
+    let taSib = document.getElementById('ta-sib');
+    let taText = e.target.value;
+    if(taText)
+    taSib.style.height = 'initial';
+    if(taText[taText.length - 1] === '\n')
+      taText += '\n'
+    taSib.innerHTML = taText;
+    if(taSib.innerHTML)
+      ta.style.height = taSib.clientHeight + 'px';
+  }
+  
   return (
-    <div>
-      <h1>Home</h1>
+    <div id="main">
+      <div>Home</div>
+        <form id="tweet-form">
+          <div id="ta-div">
+            <textarea id="ta" placeholder="What's happening?" rows="1" onChange={onChange}/>
+            <div id="ta-sib"></div>
+          </div>
+          <div id="submit-div">
+            <input type="submit" value="Tweet"/>
+          </div>
+        </form>
+      {/*}
       <p id="hello">Hello, {props.username}!</p>
       <form action="/logout" method="post">
         <input type="submit" value="Logout"/>
       </form>
+      {*/}
     </div>
   );
 }
