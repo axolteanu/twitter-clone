6cb6b8e4fa3142451055244d4a8d895754dbd049
axolteanu <alex.m.olteanu@gmail.com>
2022-08-20 20:24:52 -0400

    Refactor tweet form the React way.

--- views/react/Home.js
+++ views/react/Home.js
@@ -1,40 +1,45 @@
 import ReactDOM from 'react-dom/client';
-import React, { useEffect } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import './Home.css';
 
 function Home(props){
   
+  const tweetForm = useRef();
+  const tweetTextArea = useRef();
+  const tweetTextDiv = useRef();
+  const [tweet, setTweet] = useState('');
+
   useEffect(() => {
-    let ta = document.getElementById('ta');
-    let taSib = document.getElementById('ta-sib');
-    if(!ta.value)
-    taSib.style.height = ta.clientHeight + 'px';
+    if(tweet)
+      tweetTextDiv.current.style.height = 'initial';
+    else
+      tweetTextDiv.current.style.height = tweetTextArea.current.clientHeight + 'px';
+    
+    let txtDivVal = tweet; 
+    if(tweet[tweet.length - 1] === '\n')
+      txtDivVal += '\n';
+    tweetTextDiv.current.innerHTML = txtDivVal;
+
+    if(tweetTextDiv.current.innerHTML)
+    tweetTextArea.current.style.height = tweetTextDiv.current.clientHeight + 'px';
   });
 
   function onChange(e){
-    let taSib = document.getElementById('ta-sib');
-    let taText = e.target.value;
-    if(taText)
-    taSib.style.height = 'initial';
-    if(taText[taText.length - 1] === '\n')
-      taText += '\n'
-    taSib.innerHTML = taText;
-    if(taSib.innerHTML)
-      ta.style.height = taSib.clientHeight + 'px';
+    setTweet(e.target.value);
   }
   
   return (
     <div id="main">
       <div>Home</div>
-        <form id="tweet-form">
-          <div id="ta-div">
-            <textarea id="ta" placeholder="What's happening?" rows="1" onChange={onChange}/>
-            <div id="ta-sib"></div>
-          </div>
-          <div id="submit-div">
-            <input type="submit" value="Tweet"/>
-          </div>
-        </form>
+      <form ref={tweetForm} className="tweet-form">
+        <div className="ta-div">
+          <textarea ref={tweetTextArea} className="ta" placeholder="What's happening?" rows="1" value={tweet} onChange={onChange}/>
+          <div ref={tweetTextDiv} className="ta-sib"></div>
+        </div>
+        <div className="submit-div">
+          <input type="submit" value="Tweet"/>
+        </div>
+      </form>
       {/*}
       <p id="hello">Hello, {props.username}!</p>
       <form action="/logout" method="post">
